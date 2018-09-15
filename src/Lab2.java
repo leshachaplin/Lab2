0fc6f8e6d2ac69888948e1e48e4e4c8be56f827e
Lesha Chaplin <lesha.chaplin66@gmail.com>
2018-09-15 16:23:11 +0300

    add matrix symmetric check implementation

--- src/Lab2.java
+++ src/Lab2.java
@@ -4,49 +4,91 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Lab2 {
-    private static int[][] matrixFileInput(String nameOfFile) {
-        try {
-            FileReader fr = new FileReader(nameOfFile);
-            BufferedReader isr = new BufferedReader(fr);
 
-            final int m = Integer.parseInt(isr.readLine());
-            final int n = Integer.parseInt(isr.readLine());
+    public int getMatrixMinNorma(@NotNull int[][] matrix) {
+        int norma = Integer.MAX_VALUE;
+        for (int i = 0, size = matrix.length; i < size; i++) {
+            int sumPerLine = 0;
 
-            int[][] matrix = new int[m][n];
-
-            for (int i = 0; i < m; i++) {
-                String matrixLineStr = isr.readLine();
-                String[] stringArray = matrixLineStr.split(" ");
-                for (int j = 0, size = stringArray.length; j < size; j++) {
-                    matrix[i][j] = Integer.parseInt(stringArray[j]);
-                }
+            for (int j = 0, innerSize = matrix[i].length; j < innerSize; j++) {
+                sumPerLine += matrix[i][j];
             }
 
-            return matrix;
-        } catch (Exception e) {
-            e.printStackTrace();
+            if (norma > sumPerLine) {
+                norma = sumPerLine;
+            }
         }
 
-        return null;
+        return norma;
     }
 
-    public static void printMatrix(@NotNull int[][] matrix) {
+    /*
+    for (int i = 0, size = matrix.length; i < size; i++) {
+            for (int j = 0, innerSize = matrix[i].length; j < innerSize; j++) {
+
+            }
+        }
+    */
+
+    public void removeLocalMaximums(int[][] matrix) {
+        for (int i = 0, size = matrix.length; i < size; i++) {
+            loop:
+            for (int j = 0, innerSize = matrix[i].length; j < innerSize; j++) {
+                final int currentItem = matrix[i][j];
+                // check all neighbours
+                for (int k = Math.max(0, i - 1); k <= Math.min(i + 1, size - 1); k++) {
+                    for (int m = Math.max(0, j - 1); m <= Math.min(j + 1, innerSize - 1); m++) {
+                        if (k == i && m == j) {
+                            continue; // ignore current item
+                        }
+                        if (currentItem <= matrix[k][m]) {
+                            continue loop; // not a local maximum, go to next item
+                        }
+                    }
+                }
+                // as we reach here, it means it is a local maximum
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    public boolean checkIsSymmetricMatrix(int[][] matrix) {
         for (int i = 0, size = matrix.length; i < size; i++) {
             for (int j = 0, innerSize = matrix[i].length; j < innerSize; j++) {
-                System.out.print(matrix[i][j] + " ");
+                if (i == j) {
+                    continue; // skip main diagonal
+                }
+
+                if (matrix[i][j] != matrix[j][i]) {
+                    return false;
+                }
             }
-            System.out.println();
         }
+        return true;
     }
 
     public static void main(String[] args) {
-        int[][] matrix = matrixFileInput("matrix.txt");
+        int[][] matrix = MatrixUtils.matrixFileInput("matrix.txt");
 
         if (matrix == null) {
             System.out.println("Error while reading from file");
             return; // early exit
         }
 
-        printMatrix(matrix);
+        System.out.println("Matrix:");
+        MatrixUtils.printMatrix(matrix);
+
+        Lab2 lab = new Lab2();
+
+        System.out.println("Matrix norma:");
+        System.out.println(lab.getMatrixMinNorma(matrix));
+
+        System.out.println("Matrix without local maximums:");
+        int[][] matrixCopy = MatrixUtils.copyMatrix(matrix);
+        lab.removeLocalMaximums(matrixCopy);
+        MatrixUtils.printMatrix(matrixCopy);
+        System.out.println("Matrix symmetric:");
+        boolean isSymmetric = lab.checkIsSymmetricMatrix(matrixCopy);
+        System.out.println(isSymmetric);
     }
 }
---
+++ src/MatrixUtils.java
@@ -0,0 +1,54 @@
+import com.sun.istack.internal.NotNull;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class MatrixUtils {
+
+    public static int[][] matrixFileInput(String nameOfFile) {
+        try {
+            FileReader fr = new FileReader(nameOfFile);
+            BufferedReader isr = new BufferedReader(fr);
+
+            final int m = Integer.parseInt(isr.readLine());
+            final int n = Integer.parseInt(isr.readLine());
+
+            int[][] matrix = new int[m][n];
+
+            for (int i = 0; i < m; i++) {
+                String matrixLineStr = isr.readLine();
+                String[] stringArray = matrixLineStr.split(" ");
+                for (int j = 0, size = stringArray.length; j < size; j++) {
+                    matrix[i][j] = Integer.parseInt(stringArray[j]);
+                }
+            }
+
+            return matrix;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void printMatrix(@NotNull int[][] matrix) {
+        for (int i = 0, size = matrix.length; i < size; i++) {
+            for (int j = 0, innerSize = matrix[i].length; j < innerSize; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] copyMatrix(int[][] matrix) {
+        int[][] newMatrix = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0, size = newMatrix.length; i < size; i++) {
+            for (int j = 0, innerSize = newMatrix[i].length; j < innerSize; j++) {
+                newMatrix[i][j] = matrix[i][j];
+            }
+        }
+
+        return newMatrix;
+    }
+}
